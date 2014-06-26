7e13cc5d9736e3d2c0ed74314dfe6c4338ce87af
Burak Sezer <bburaksezer@gmail.com>
2014-06-26 20:54:27 +0300

    move actions a seperated file

---
+++ newton/actions.go
@@ -0,0 +1,134 @@
+package newton
+
+import (
+	"encoding/json"
+	"errors"
+	"github.com/nu7hatch/gouuid"
+	"github.com/purak/newton/cstream"
+	"github.com/purak/newton/message"
+	"github.com/purak/newton/store"
+	"net"
+	"time"
+)
+
+// Create a new client session
+func (n *Newton) createSession(data map[string]interface{}, conn *net.Conn) ([]byte, error) {
+	clientId, ok := data["ClientId"].(string)
+	if !ok {
+		return nil, errors.New("ClientId doesn't exist or invalid.")
+	}
+
+	remoteAddr := (*conn).RemoteAddr().String()
+	clientIp, err := cstream.ParseIP(remoteAddr)
+	if err != nil {
+		return nil, err
+	}
+
+	// AUTHENTICATION HERE
+
+	now := time.Now().Unix()
+	// Go's maps are not thread-safe
+	n.ConnTable.RLock()
+	_, ok = n.ConnTable.m[clientId]
+	n.ConnTable.RUnlock()
+
+	if ok {
+		// Remove the previous connection
+		n.ConnClientTable.Lock()
+		delete(n.ConnClientTable.c, conn)
+		n.ConnClientTable.Unlock()
+
+		n.ConnTable.Lock()
+		delete(n.ConnTable.m, clientId)
+		n.ConnTable.Unlock()
+
+		return nil, errors.New("Client has an active connection.")
+	}
+
+	// Create a new clientItem
+	expireAt := time.Now().Unix() + n.Config.Server.ClientAnnounceInterval
+	secret, err := uuid.NewV4()
+	if err != nil {
+		return nil, err
+	}
+
+	clientItem := &ClientItem{
+		Ip:            clientIp,
+		LastAnnounce:  now,
+		SessionSecret: secret.String(),
+		Conn:          conn,
+	}
+
+	// Add a new item to priority queue
+	item := &store.Item{
+		Value: clientId,
+		TTL:   expireAt,
+	}
+	n.ClientQueue <- item
+
+	// Set to ConnTable
+	n.ConnTable.Lock()
+	n.ConnTable.m[clientId] = clientItem
+	n.ConnTable.Unlock()
+
+	// Set to ConnClientTable
+	n.ConnClientTable.Lock()
+	n.ConnClientTable.c[conn] = clientItem
+	n.ConnClientTable.Unlock()
+
+	msg := &message.Authenticated{
+		Type:          "Authenticated",
+		SessionSecret: secret.String(),
+	}
+
+	b, err := json.Marshal(msg)
+	if err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
+
+// Creates a new user
+func (n *Newton) createUser(data map[string]interface{}) ([]byte, error) {
+	// Check username
+	username, ok := data["Username"].(string)
+	if !ok {
+		return nil, errors.New("Username is required.")
+	}
+
+	// Check password
+	password, ok := data["Password"].(string)
+	if !ok {
+		return nil, errors.New("Password is required.")
+	}
+
+	_, existed := n.UserStore.Get(username)
+	if !existed {
+		// Finally, create a new user
+		err := n.UserStore.Create(username, password)
+
+		if err != nil {
+			return nil, err
+		}
+
+		clientId, err := n.UserStore.CreateUserClient(username)
+		if err != nil {
+			return nil, err
+		}
+
+		msg := &message.ClientId{
+			Type:     "ClientId",
+			ClientId: clientId,
+		}
+
+		b, err := json.Marshal(msg)
+		if err != nil {
+			return nil, err
+		}
+
+		return b, nil
+	} else {
+		return nil, errors.New("Already exist.")
+	}
+}
