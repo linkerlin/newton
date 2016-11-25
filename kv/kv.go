ccc7cee9530acd3131190970ec42e17457f5cbdb
Burak Sezer <bburaksezer@gmail.com>
2016-11-26 00:54:33 +0300

    kv: improve code organization

--- kv/kv.go
+++ kv/kv.go
@@ -1,11 +1,9 @@
 package kv
 
 import (
-	"errors"
 	"sync"
 
 	"github.com/julienschmidt/httprouter"
-	"github.com/purak/newton/log"
 	"github.com/purak/newton/partition"
 	"github.com/spaolacci/murmur3"
 )
@@ -26,18 +24,6 @@ type KV struct {
 	backups    *partitions
 }
 
-type partitions struct {
-	mu sync.RWMutex
-
-	m map[int32]*kv
-}
-
-type kv struct {
-	mu sync.RWMutex
-
-	m map[string]*item
-}
-
 func New(p *partition.Partition, router *httprouter.Router) *KV {
 	parts := &partitions{
 		m: make(map[int32]*kv),
@@ -53,78 +39,15 @@ func New(p *partition.Partition, router *httprouter.Router) *KV {
 }
 
 func (k *KV) Set(key string, value []byte) error {
-	// Find partition number for the given key
-	partID := getPartitionID(key)
-	// TODO: find responsible node for that key. Redirect or set locally.
-
-	k.partitions.mu.RLock()
-	part, ok := k.partitions.m[partID]
-	if !ok {
-		part = &kv{
-			m: make(map[string]*item),
-		}
-		k.partitions.m[partID] = part
-	}
-	k.partitions.mu.RUnlock()
-
-	part.mu.RLock()
-	i, ok := part.m[key]
-	part.mu.RUnlock()
-	if ok {
-		// Update the value in source an its backups.
-		i.mu.Lock()
-		i.value = value
-		// TODO: Update backups
-		i.mu.Unlock()
-		return nil
-	}
-	// Create a new record.
-	i = &item{
-		value: value,
-	}
-	i.mu.Lock()
-
-	part.mu.Lock()
-	part.m[key] = i
-	part.mu.Unlock()
-
-	// TODO: update backups
-	i.mu.Unlock()
-	log.Infof("Set operation has been done for key %s", key)
-	return nil
+	return k.partitions.set(key, value)
 }
 
-var ErrKeyNotFound = errors.New("No value found for given key")
-
 func (k *KV) Get(key string) ([]byte, error) {
-	// Find partition number for the given key
-	partID := getPartitionID(key)
-	// TODO: find responsible node for that key. Redirect or get locally.
+	return k.partitions.get(key)
+}
 
-	k.partitions.mu.RLock()
-	part, ok := k.partitions.m[partID]
-	if !ok {
-		k.partitions.mu.RUnlock()
-		// Partition could not be found for that key.
-		return nil, ErrKeyNotFound
-	}
-	k.partitions.mu.RUnlock()
-
-	part.mu.RLock()
-	i, ok := part.m[key]
-	part.mu.RUnlock()
-	if !ok {
-		return nil, ErrKeyNotFound
-	}
-
-	// Create a thread-safe copy and unlock the data structure
-	i.mu.RLock()
-	value := make([]byte, len(i.value))
-	copy(value, i.value)
-	i.mu.RUnlock()
-
-	log.Infof("Get operation has been done for key %s", key)
-	return value, nil
+func (k *KV) Delete(key string) error {
+	return k.partitions.delete(key)
 }
 
 func (k *KV) Stop() {
